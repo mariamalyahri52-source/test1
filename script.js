6a4b6274563fb9957dde0ccc53c2e549eb28a444
mariamalyahri52-source <mariamalyahri52@gmail.com>
2025-10-19 13:48:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById("contactform").onsubmit = function () {
+  alert("Your inquiry has been submitted. ");
+  return false; 
+};
 
+document.getElementsByTagName("tr")[1].onclick = function () {
+  alert("Candles Sold Out");
+};
